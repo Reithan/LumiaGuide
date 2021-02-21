c3a1dfd38fe971cc84e82758195c926910f0196c
Bryan O'Malley <bo122081@hotmail.com>
2021-02-21 13:27:17 -0800

    move layout into App

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="main wrapper">
+      <div class="main pane">
+        <div class="left pane">
+          <div class="build gear pane">
+            <h1>Display Item Build Here</h1>
+          </div>
+          <div class="build consumables pane">
+            <h1>Display List of Consumables on Path Here</h1>
+          </div>
+          <div class="build stats pane">
+            <h1>Display Stats for Build Here</h1>
+          </div>
+        </div>
+        <div class="center pane">
+          <div class="shop pane">
+            <h1>Display Searchable List of All Items Here</h1>
+          </div>
+          <div class="plan pane">
+            <h1>Display Path Options Here</h1>
+          </div>
+        </div>
+        <div class="right pane">
+          <div class="map pane">
+            <h1>Display Map & Zone Options Here</h1>
+          </div>
+          <div class="item-add pane">
+            <h1>Display List of Additional Items on Path Here</h1>
+          </div>
+        </div>
+      </div>
+    </div>  
   );
 }
 
