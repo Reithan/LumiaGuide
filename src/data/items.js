58fc034316ec010d7c9277405e580ee938f5803d
Bryan O'Malley <bo122081@hotmail.com>
2021-02-21 23:14:11 -0800

    Add items aggregation

--- src/data/items.js
+++ src/data/items.js
@@ -1,3 +1,15 @@
-import * as Weapons from './weapons.js'
-import * as Armors from './armors.js'
-import * as Consumables from './consumables.js'
+import { weapons } from './weapons.js';
+import { armors } from './armors.js';
+import { consumables } from './consumables.js';
+import { summons } from './summons.js';
+
+export var all_items = {};
+var item_collections = [weapons, armors, consumables, summons];
+
+for (const collection of item_collections) {
+  for (const item in collection) {
+    if (Object.hasOwnProperty.call(collection, item)) {    
+      all_items[item] = collection[item];
+    }
+  }  
+}
