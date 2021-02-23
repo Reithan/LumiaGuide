0639bfcfffed440e5e4a98fa48a5c662fedfebd9
Bryan O'Malley <bo122081@hotmail.com>
2021-02-22 18:11:27 -0800

    Add check for duplicate items

--- src/data/items.js
+++ src/data/items.js
@@ -2,13 +2,17 @@ import { weapons } from './weapons.js';
 import { armors } from './armors.js';
 import { consumables } from './consumables.js';
 import { summons } from './summons.js';
+import { materials } from './materials.js';
 
 export var all_items = {};
-var item_collections = [weapons, armors, consumables, summons];
+var item_collections = [weapons, armors, consumables, summons, materials];
 
 for (const collection of item_collections) {
   for (const item in collection) {
-    if (Object.hasOwnProperty.call(collection, item)) {    
+    if (Object.hasOwnProperty.call(collection, item)) {
+      if(all_items[item] != null) {
+        throw new MediaError("Item exists twice in data.");
+      }
       all_items[item] = collection[item];
     }
   }  
