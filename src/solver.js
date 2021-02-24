3cec8d71659e0b0c3a830031b89d108a64968712
Bryan O'Malley <bo122081@hotmail.com>
2021-02-23 20:20:46 -0800

    Fix all known bugs in inventory class

--- src/solver.js
+++ src/solver.js
@@ -23,50 +23,81 @@ export class Inventory {
   #inventory = [];
   #gear_slots = {};
 
-  size() { return inventory.size(); }
+  size() { return this.#inventory.length; }
 
   getGearStats(type) {
     if(Util.toType(type) != "string" || !ItemClass.GearType.includes(type)) {
       throw new TypeError("getGearStats takes a string specifying a GearType as an argument.");
     }
-    if(this.gear_slots[type] == undefined || this.gear_slots[type] == null) {
+    if(this.#gear_slots[type] == undefined || this.#gear_slots[type] == null) {
       return null;
     }
-    return this.gear_slots[type];
+    return this.#gear_slots[type];
+  }
+
+  getInventory() { return [...this.#inventory]; }
+
+  getAllItems() {
+    var out_items = [];
+    for (const geartype in this.#gear_slots) {
+      if (Object.hasOwnProperty.call(this.#gear_slots, geartype) && this.#gear_slots[geartype] != null && this.#gear_slots[geartype] != undefined) {
+        const gear = this.#gear_slots[geartype];
+        out_items.push([gear,1]);
+      }
+    }
+    for (const inv_item of this.#inventory) {
+      var found = false;
+      for (const out_item of out_items) {
+        if(out_item[0] === inv_item[0]) {
+          out_item[1] += inv_item[1];
+          found = true;
+        }
+      }
+      if(!found) {
+        out_items.push([...inv_item]);
+      }
+    }
+    return out_items;
   }
 
   cleanInventory() {
     for (const geartype of ItemClass.GearType) {
-      if(this.gear_slots[geartype] == null || this.gear_slots[geartype] == undefined) {
+      if(this.#gear_slots[geartype] == null || this.#gear_slots[geartype] == undefined) {
         var best = null;
-        for (const slot of this.inventory) {
+        for (const slot of this.#inventory) {
           if(slot[0].type == geartype) {
-            if(best == null || (slot[1] > 0 && ItemClass.ItemRarity.indexOf(best.rarity) < ItemClass.ItemRarity.indexOf(slot[0].rarity))) {
+            if(best == null || (slot[1] > 0 && ItemClass.ItemRarity.indexOf(best[0].rarity) < ItemClass.ItemRarity.indexOf(slot[0].rarity))) {
               best = slot;
             }
           }
         }
         if(best != null) {
-          this.gear_slots[geartype] = best[0];
+          this.#gear_slots[geartype] = best[0];
           --best[1];
         }
       }
     }
 
-    inventory = this.inventory.filter(slot => slot[1] > 0);
+    this.#inventory = this.#inventory.filter(slot => slot[1] > 0);
   }
 
-  addItem(itemstats, number) {
-    if(!((item[0] instanceof ItemClass.ItemStats) && Number.isInteger(item[1]))) {
-      throw new TypeError("addItems takes an array of item/quantity pairs as an argument.");
+  addItem(itemstats, number = 1) {
+    if(!((itemstats instanceof ItemClass.ItemStats) && Number.isInteger(number))) {
+      throw new TypeError("addItem takes an item and optional quantity as an arguments.");
     }
 
     if(itemstats instanceof ItemClass.GearStats) {
-      if(number > 0 && this.gear_slots[itemstats.type] == null || this.gear_slots[itemstats.type] == undefined) {
-        this.gear_slots[itemstats.type] = itemstats;
-        --number;
-      } else if (number < 0 && this.gear_slots[itemstats.type] != null && this.gear_slots[itemstats.type] != undefined) {
-        this.gear_slots[itemstats.type] = null;
+      if(number > 0) {
+        if(this.#gear_slots[itemstats.type] == null || this.#gear_slots[itemstats.type] == undefined) {
+          this.#gear_slots[itemstats.type] = itemstats;
+          --number;
+        } else if (ItemClass.ItemRarity.indexOf(this.#gear_slots[itemstats.type].rarity) < ItemClass.ItemRarity.indexOf(itemstats.rarity)) {
+          this.addItem(this.#gear_slots[itemstats.type]);
+          this.#gear_slots[itemstats.type] = itemstats;
+          --number;
+        }
+      } else if (number < 0 && (this.#gear_slots[itemstats.type] != null && this.#gear_slots[itemstats.type] != undefined)) {
+        this.#gear_slots[itemstats.type] = null;
         ++number;
       }
       if(number == 0) {
@@ -75,8 +106,8 @@ export class Inventory {
       }
     }
 
-    for (const slot of inventory) {
-      if(slot[0] == itemstats.name) {
+    for (const slot of this.#inventory) {
+      if(slot[0] == itemstats) {
         if(slot[1]+number <= itemstats.max_stack && slot[1]+number >= 0) {
           slot[1] += number;
           this.cleanInventory();
@@ -95,7 +126,7 @@ export class Inventory {
       if(new_stack > itemstats.max_stack) {
         new_stack = itemstats.max_stack;
       }
-      this.inventory.push([itemstats.name,new_stack]);
+      this.#inventory.push([itemstats,new_stack]);
       number -= new_stack;
     }
 
@@ -109,22 +140,26 @@ export class Inventory {
     }
     var remaining = 0;
     for (const item of itemlist) {
-      remaining += this.addItem(item);
+      if(Util.toType(item) == "array") {
+        remaining += this.addItem(item[0], item[1]);
+      } else {
+        remaining += this.addItem(item);
+      }
     }
     return remaining;
   }
 
   haveItem(itemname, quantity = 1) {
-    for (const slot in this.gear_slots) {
-      if (Object.hasOwnProperty.call(this.gear_slots, slot)) {
-        const gear = this.gear_slots[slot];
+    for (const slot in this.#gear_slots) {
+      if (Object.hasOwnProperty.call(this.#gear_slots, slot)) {
+        const gear = this.#gear_slots[slot];
         if(gear != undefined && gear != null && gear.name == itemname) {
           --quantity;
         }
       }
     }
-    for (const slot of this.inventory) {
-      if(slot[0] == itemname) {
+    for (const slot of this.#inventory) {
+      if(slot[0].name == itemname) {
         quantity -= slot[1];
       }
     }
@@ -135,10 +170,12 @@ export class Inventory {
     if(!(itemstats.recipe instanceof ItemClass.ItemRecipe)) {
       throw new TypeError("craftItem takes an ItemStats with a defined ItemRecipe as an argument.");
     }
-    if(this.haveItem(itemstats.recipe[0]) && this.haveItem(itemstats.recipe[1])) {
-      this.addItem(itemstats.recipe[0], -1);
-      this.addItem(itemstats.recipe[1], -1);
-      this.addItem(itemstats);
+    var part1 = Items.all_items[itemstats.recipe.part1];
+    var part2 = Items.all_items[itemstats.recipe.part2];
+    if(this.haveItem(part1.name) && this.haveItem(part2.name)) {
+      this.addItem(part1, -1);
+      this.addItem(part2, -1);
+      this.addItem(itemstats, itemstats.quantity);
       this.cleanInventory();
       return true;
     }
