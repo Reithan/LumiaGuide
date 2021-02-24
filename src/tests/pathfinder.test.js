541e0b1b4abe9fc5e981d9138cd567617e56179a
Bryan O'Malley <bo122081@hotmail.com>
2021-02-23 21:59:39 -0800

    add test for shopping list

--- src/tests/pathfinder.test.js
+++ src/tests/pathfinder.test.js
@@ -9,3 +9,32 @@ test('Setting goal requires <=6 items with <=1 of each type.', () => {
   expect(pathfinder.setGoal(["Brass Knuckles","Close Helm","Bulletproof Vest","Sheath","Heelys","Glass Pieces","Stone"])).toBe(false);
   expect(pathfinder.setGoal(["Brass Knuckles","Close Helm","Bulletproof Vest","Sheath","Heelys","Stone"])).toBe(false);
 });
+
+test('Shopping list is populated by goal items\' constituent parts.', () => {
+  var pathfinder = new Pathfinder.Pathfinder();
+  pathfinder.setGoal(["Brass Knuckles","Heelys"]);
+  var shoppinglist = pathfinder.getCurrentShoppingList();
+  expect(shoppinglist).toHaveLength(3);
+  expect(shoppinglist).toContainEqual(["Brass Knuckles",1]);
+  expect(shoppinglist).toContainEqual(["Running Shoes",1]);
+  expect(shoppinglist).toContainEqual(["Iron Ball",1]);
+
+  pathfinder.setGoal(["Leather Gloves","Heelys","Leather Shield"]);
+  shoppinglist = pathfinder.getCurrentShoppingList();
+  expect(shoppinglist).toHaveLength(5);
+  expect(shoppinglist).toContainEqual(["Cotton Gloves",1]);
+  expect(shoppinglist).toContainEqual(["Leather",2]);
+  expect(shoppinglist).toContainEqual(["Running Shoes",1]);
+  expect(shoppinglist).toContainEqual(["Iron Ball",1]);
+  expect(shoppinglist).toContainEqual(["Turtle Shell",1]);
+
+  pathfinder.setGoal(["Bracelet of Skadi"]);
+  shoppinglist = pathfinder.getCurrentShoppingList();
+  expect(shoppinglist).toHaveLength(6);
+  expect(shoppinglist).toContainEqual(["Bracelet",1]);
+  expect(shoppinglist).toContainEqual(["Mousetrap",1]);
+  expect(shoppinglist).toContainEqual(["Nail",1]);
+  expect(shoppinglist).toContainEqual(["Tree of Life",1]);
+  expect(shoppinglist).toContainEqual(["Stone",1]);
+  expect(shoppinglist).toContainEqual(["Ice",1]);
+});
