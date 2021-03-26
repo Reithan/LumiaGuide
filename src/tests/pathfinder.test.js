5b19341ca06c350a695d6694e237da37e0db1f0a
Reithan <bo122081@hotmail.com>
2021-03-26 12:34:47 -0700

    ERBS Updates (#12)
    * 0.27 weapons
    
    * 0.27 armours
    
    * 0.27 food
    
    * 0.27 traps
    
    * 0.27 area drops
    
    * Update tests 0.27
    
    * 0.27.2 Item changes
    
    * Update area drops 0.27.2
    
    * 0.28 weapons
    
    * Update stats tests
    
    * 0.28 armours
    
    * 0.28 traps
    
    * Area drops 0.28
    
    * 0.28.1 weapons
    
    * Stats tests update
    
    * Armour 0.28.2
    
    * Area Drops 0.28.2

--- src/tests/pathfinder.test.js
+++ src/tests/pathfinder.test.js
@@ -155,9 +155,9 @@ test('Test full area collect function', () => {
   var itemlist = makeItemList(pathfinder);
   
   const expected_itemlist = [["Kitchen Knife",1],["Branch",7],["Bandage",7],["Binoculars",7],
-      ["Iron Ball",7],["Chalk",7],["Short Crossbow",4],["Walther PPK",4],["Fedorova",4],
+      ["Iron Ball",7],["Chalk",7],["Short Crossbow",3],["Walther PPK",4],["Fedorova",4],
       ["Hatchet",4],["Curry Powder",6],["Stone",4],["Nail",8],["Turtle Shell",8],["Scrap Metal",10],
-      ["Lighter",10],["Battery",10],["Alcohol",6],["Oil",8],["Glue",7],["Leather",9],["Meat",5]];
+      ["Lighter",10],["Battery",10],["Alcohol",6],["Oil",8],["Glue",8],["Leather",9],["Meat",5]];
 
   const sortlistalphabetically = (e1,e2) => e1[0] > e2[0] ? 1 : e1[0] < e2[0] ? -1 : 0;
   itemlist.sort(sortlistalphabetically);
